5ca63e39e4615a677a02932241e2915c94db2943
Amos Brocco <amos.brocco@gmail.com>
2023-03-03 09:48:13 +0100

    End of introduction

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,76 @@
-#include "hello.h"
 #include <iostream>
+#include <string>
 
-void sayHi() {
-    std::cout << "Hi moon\n";
+// Forward declarations
+double average(double a, double b);
+double average(double a, double b, double c);
+// Default values
+// --> always in the declaration!
+double sum(double a, double b = 0, double c = 0);
+// This will clash with double sum(double a, double b, double c = 0)
+/*double sum(double a, double b) {
+
+}*/
+
+void fun(int a, double b);
+void fun(double a, int b);
+
+double average(int a, int b) {
+    std::cout << "Average int,int\n";
+    return (a+b)/2;
 }
 
-//namespace supsi {
-//    namespace cppcourse {
-// ===
-namespace supsi::cppcourse {
-    void sayHi() {
-        std::cout << "Hi jupyter\n";
-        ::sayHi(); // This calls sayHi from the global namespace
-        hellolib::sayHi(); // This calls sayHi from the hellolib namespace
-    }
-
-    void sayHello() {
-        std::cout << "Hello again\n";
-    }
+double sum(double a, double b, double c) {
+    return a+b+c;
+}
+int f(int) {
+    return 3;
 }
 
-namespace hellolib {
-    void sayCiao() {
-        std::cout << "Ciao\n";
-    }
+double f(double) {
+    return 3;
 }
 
-namespace debug {
-    void sayHello() {
-        std::cout << "Calling sayHello\n";
-        hellolib::sayHello();
-    }
 
-    void sayHi() {
-        std::cout << "Calling sayHi\n";
-        hellolib::sayHi();
-    }
-}
+// NOT a valid overload (cannot overload on the return value)
+/*int average(double a, double b) {
+    return (a+b)/2;
+}*/
 
-// Namespace Alias 
-namespace mn = debug;
-
-
-
-int main()
-{
-    //mn::sayHello();
-    //mn::sayHi();
-
-    using hellolib::sayHello;
-    sayHello();
-
+int main() {
     using namespace std;
-    cout << "End of this class\nHave a nice weekend\n";
+    cout << "The average of 2 and 5 is " << average(2, 5) << "\n";
+    cout << "The average of 2.5 and 5.1 is " << average(2.5, 5.1) << "\n";
+    cout << "The sum of 3.14 and 2 and 1 is " << sum(3.14, 2, 1) << "\n";
+    cout << "The sum of 3.14 and 2 is " << sum(3.14, 2) << "\n";
+    cout << "The sum of 3.14 is " << sum(3.14) << "\n";
+    //fun(3,double(4));
+    //fun(int(3.5),5.6);
+    f(3);
+
+    //int pi = 3.14;
+    //int pi2(3.14);
+    double e{2.6}; // C++ 11 way of initializing things
+    // e{2.62}; // This will not compile
+    auto pi{3.14};
+
+    auto x{f(5.5)};
+
+    string message = "Hello world";
+    cout << message << ", " << "Length: " << message.length() << " is empty?" << message.empty() << "\n";
+    string message2 = message + "!";
+    cout << message2 << ", " << "Length: " << message2.length() << " is empty?" << message2.empty() << "\n";
+    cout << message2[0] << message[1] << "\n";
+
+    
 }
 
+double average(double a, double b) {
+    std::cout << "Average double,double\n";
+    return (a+b)/2;
+}
 
+double average(double a, double b, double c) {
+    std::cout << "Average double,double,double\n";
+    return (a+b+c)/3;
+}
